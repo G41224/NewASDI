5d1a91cc7084a00bdee6dc2932b2b786715b404d
Zachery croasmun <th3p4ler1der@gmail.com>
2013-12-08 20:10:58 -0800

    final commit for week 2

--- JS/Main.js
+++ JS/Main.js
@@ -1,3 +1,115 @@
 /**
  * @author Zac Croasmun
  */
+
+
+$(function(){
+	
+	var parseAddForm = function (data) {
+  
+  //console.log(data);
+};
+	
+	var addForm = $("#bookForm"),
+		addErrLink = $("#addErrLink")
+	;
+	
+	addForm.validate({
+		invalidHandler: function(form, validator){
+			addErrLink.click();
+			var html = "";
+			for(var key in validator.submitted){
+				var label = $("label[for^='"+key+"']").not("[generated]");
+				var legend = label.closest("fieldset").find(".ui-controlgroup-label");
+				var fieldName = legend.lenght ? legend.text() : label.text();
+				html += "<li>" + fieldName + "</li>";
+			};
+			$("#formErrors ul").html(html);
+			
+		},
+		submitHandler: function(){
+			var data = addForm.serializeArray();
+			parseAddForm(data);
+			
+			}
+		
+		
+	});
+	
+	function store(key){
+		if(!key){
+		var id = Math.floor(Math.random()*10000001);
+		}else{
+			id = key;
+		}
+		
+		var listItem = {};
+			listItem.Title =       [$("#bTitle").val()];
+			listItem.Author =       [$("#bAuthor").val()];
+			listItem.Discription =        [$("#bDisc").val()];
+			
+			
+			localStorage.setItem(id, JSON.stringify(listItem));
+			alert("book Saved!");
+			window.location.reload();
+			
+	}
+	
+	
+	function autoFill(){
+		for(var n in json){
+			var id = Math.floor(Math.random()*10000001);
+			localStorage.setItem(id, JSON.stringify(json[n]));
+			
+		}
+		
+	}
+	
+	
+	
+	function display(){
+		if(localStorage.length === 0){
+			var conf = confirm("do you want to add some")
+			if(conf===true){
+				autoFill();
+			}
+			else{
+				
+			}
+			
+		}else{
+			var storage = window.localStorage;
+			var List = "<h3>BOOKS</h3>";
+			for(var key in storage ){
+				var bookName = storage[key];
+				
+				List += "<ul>"+
+						"<li>"+
+						bookName+
+						autoFill();+
+						"</li>"+
+						"</ul>";
+						
+						$("#bookList").html(List);
+			}	
+		}	
+	}
+	function clearData () {
+	  if(localStorage.length === 0){
+	  alert("no data")
+	  }else{ 
+	  	localStorage.clear();
+	  	alert("books deleted!");
+	  	window.location.reload();
+	  	return false;
+	   }
+	}
+	
+	
+	$("#bookForm").on("submit", store)
+	$("#clearStorage").click(clearData)
+	display();
+	
+	console.log(localStorage);
+	
+});
---
+++ JS/json.js
@@ -0,0 +1,31 @@
+var json = {
+
+	"Book1":{
+		"bTitle":["Title:", "moby dick"],
+		"bAuthor":["Author:", "Herman Melville"],
+		"bDisc":["Discription:", "an epic sea-story of Captain Ahab's voyage in pursuit of Moby Dick, a great white whale."]
+		},
+	
+	"Book2":{
+		"bTitle":["Title:", "The Adventures of Huckleberry Finn"],
+		"bAuthor":["Author:", "Mark Twaine"],
+		"bDisc":["Discription:", "sa sequal to The Adventures of Tom Sawyer "]
+		},
+	"Book3":{
+		"bTitle":["Title:", "Hamlet"],
+		"bAuthor":["Author:", "William Shakespeare"],
+		"bDisc":["Discription:", "Set in the Kingdom of Denmark, the play dramatizes the revenge Prince Hamlet exacts on his uncle Claudius."]
+		},
+	"Book4":{
+		"bTitle":["Title:", "The Great Gatsby"],
+		"bAuthor":["Author:", "F. Scott Fitzgerald"],
+		"bDisc":["Discription:", "follows a cast of characters living in the fictional town of West Egg on prosperous Long Island in the summer of 1922."]
+		},
+	"Book5":{
+		"bTitle":["Title:", "The Art of War"],
+		"bAuthor":["Author:", "Sun Tzu"],
+		"bDisc":["Discription:", "tactics of an ancient Chinese General"]
+		}
+	
+	
+};
